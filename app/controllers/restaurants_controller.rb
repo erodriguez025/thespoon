d453fe1a9e9c7d8a5d7f23d0098a27107b98586c
emi rodriguez <emirodriguez_025@hotmail.com>
2021-06-09 14:10:17 -0300

    Generate fake restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,14 @@
 class RestaurantsController < ApplicationController
+  
+  RESTAURANTS = {
+    1 => { name: "piba", address: "libertador 600", category: "pinchos"},
+    2 => { name: "siga la vaca", address: "parana 945", category: "parrilla"},
+    3 => { name: "kansas", address: "córdoba 900", category: "american food"},
+    4 => { name: "la farola", address: "santa fe 456", category: "pastas"}
+  }
+
+  def index
+     @restaurants = RESTAURANTS 
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "restaurants", to: "restaurants#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
