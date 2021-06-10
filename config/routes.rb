29cbeedda8d4cf2b742e5a16cc44cc4319e4993f
emi rodriguez <emirodriguez_025@hotmail.com>
2021-06-10 15:24:16 -0300

    Add links

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
   get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/:id", to: "restaurants#show", as:"restaurant"
   post "restaurants", to: "restaurants#create"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
